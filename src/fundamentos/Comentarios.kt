d265767b5563eef841feb3adde0586b5008865f4
Carlos Henrique Paes Vargas <carloshp@outlook.com>
2018-11-18 17:03:04 -0200

    inclusão do projeto no gitHub

---
+++ src/fundamentos/Comentarios.kt
@@ -0,0 +1,22 @@
+package fundamentos
+
+/**
+ * Só pra dizer que o Kotlin suporta o KDoc...
+ * @author Carlos Henrique
+ * @param args lista de parametros recebidos pelo main.
+ */
+fun main(args: Array<String>) {
+    // Comentário de uma linha
+
+    /*
+    Mais de
+    uma Linha
+     */
+
+    /**
+     * Mais
+     * de uma
+     * linha com asteristicos.
+     */
+}
+
---
+++ src/fundamentos/Const.kt
@@ -0,0 +1,7 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    val a: Int = 1
+    val b = 2
+
+}
